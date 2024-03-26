04e10db19bb71ffcb219a88a6e9e7310fe4c782f
ishayana <shayaniza@yahoo.com>
2024-03-26 23:43:03 +0330

    remove some lines from utils.py

--- userprofile/utils.py
+++ userprofile/utils.py
@@ -2,31 +2,6 @@ from django.db import models
 from PIL import Image
 import os
 
-# # resize custom ImageFields
-# def Resize_image(image_file, max_width=1000, max_hieght=1000):
-#     img = Image.open(image_file)
-#     width, height = img.size
-
-#     # Calculate image size in megabytes after extracting dimensions
-#     image_size = (width * height) / (1024 * 1024)
-#     aspect_ratio = width / height  # Calculate aspect ratio
-#     if width > max_width or height > max_hieght:
-
-#         if aspect_ratio > 1:  # Landscape orientation
-#             new_width = max_width
-#             new_height = int(max_width / aspect_ratio)
-#         else:  # Portrait or square orientation
-#             new_height = max_hieght
-#             new_width = int(max_hieght * aspect_ratio)
-
-#         resized_img = img.resize((int(new_width), int(new_height)))
-#         resized_img.save(f'{image_file}-resized.{img.format.lower()}')
-#         return resized_img
-        
-#     return img
-
-
-
 
 def Resize_image(image, max_width=1000, max_height=1000):
     img = Image.open(image)
